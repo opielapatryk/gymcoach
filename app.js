411e61cce3d15301c07d86bdfd6f3ae6471eb542
opielapatryk <xpatrykopiela@gmail.com>
2022-07-08 16:24:34 +0200

    master

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const submit = document.querySelector(".xd")
-submit.addEventListener("click", (event)=>{
-    event.preventDefault();
-    console.log('xd')
-})
+const submit = document.querySelectorAll("input")
+submit[4].addEventListener("click", (event)=>{
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData(form);
+        //open the request
+        xhr.open('POST','http://localhost:7000/tests/v1.0/form')
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        //send the form data
+        xhr.send(JSON.stringify(Object.fromEntries(formData)));
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                form.reset(); //reset form after AJAX success or do something else
+            }
+        }
+        //Fail the onsubmit to avoid page refresh.
+        return false; 
+    })
