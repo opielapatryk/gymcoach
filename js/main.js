640758b41f762b584cdbd7963242c57407940507
opielapatryk <xpatrykopiela@gmail.com>
2022-07-18 20:53:35 +0200

    master

--- js/main.js
+++ js/main.js
@@ -1,56 +1,38 @@
+//******* NAVIGATION MENU *******//
 const h1 = document.querySelectorAll('.h1')
-const Ul = document.querySelectorAll('.ul')
 h1.forEach((e)=>{
     e.addEventListener('click',()=>{
-        e.nextSibling.nextSibling.style.display==='block' ? e.nextSibling.nextSibling.style.display='none' : e.nextSibling.nextSibling.style.display='block'
+        e.nextSibling.nextSibling.style.display !== 'block' ? e.nextSibling.nextSibling.style.display = 'block' : e.nextSibling.nextSibling.style.display = 'none'
     })
 })
-const date = new Date()
-const dzisiaj = document.querySelector('.dzisiaj')
-const fullMonth = ()=>{
-    if (date.getMonth() + 1 < 10){
-        return `0${date.getMonth() + 1}`
+//******* GENERATE TABLE *******//
+//consts
+const main = document.querySelector('main')
+const generateRows = document.querySelector('.generate-rows')
+const generateCols = document.querySelector('.generate-cols')
+const generateSubmit = document.querySelector('.generate-submit').addEventListener("click",()=>generateTable(parseInt(generateRows.value),parseInt(generateCols.value)))
+
+//generate table function
+const generateTable = function(rowsQty, colsQty){
+    const table = document.createElement('table')
+    const tbody = document.createElement('tbody')
+    table.appendChild(tbody)
+    for(let i = 0; i < rowsQty; i++){
+        const tr = document.createElement('tr')
+        for(let j = 0; j < colsQty; j++){
+            const td = document.createElement('td')
+            tr.appendChild(td)
+            td.innerText = 'xd'
+        }
+        tbody.appendChild(tr)
+    }   
+    if(main.childElementCount === 0){
+        return main.appendChild(table) 
     } else {
-        return date.getMonth() + 1
+        return null
     }
 }
-dzisiaj.innerHTML = `${date.getDate()}.${fullMonth()}.${date.getFullYear()}`
 
-const addCol = document.querySelector('.add-col')
-const addRow = document.querySelector('.add-row')
-const tbody = document.querySelector('tbody') 
-const trHtml = document.querySelector('tr')
-let rows = document.querySelectorAll('tr')
-const trChildren = trHtml.children.length
-
-addRow.addEventListener('click',()=>{
-    let row = document.createElement("tr")
-    tbody.appendChild(row)
-    for(let i = 0; i< trChildren; i++){
-        let td = document.createElement('td')
-        td.innerText = 'td'
-        row.appendChild(td)
-    }
-    let n = localStorage.getItem('counterrows');
-    if (n === null) {
-        n = rows.length + 1 ;
-    } else {
-        n++;
-    }
-    console.log(n);
-    localStorage.setItem("counterrows", n);
-})
-const clearRow = document.querySelector('.clear-row')
-clearRow.addEventListener('click',()=>localStorage.clear())
-const clearColumn = document.querySelector('.clear-columns')
-clearColumn.addEventListener('click',()=>localStorage.clear())
-
-// localStorage.setItem('cols', trChildren)
-// console.log(JSON.parse(localStorage.getItem('cols')) + 1);
-addCol.addEventListener('click',()=>{
-    rows.forEach((e)=>{
-        let td = document.createElement('td')
-        td.innerText = 'td'
-        e.appendChild(td)
-    })  
-})
+//******* DELETE TABLE *******//
+const deleteTable = document.querySelector(".delete-table")
+deleteTable.addEventListener('click',()=>main.innerHTML = '')
