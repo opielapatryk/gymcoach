4c8fc651d42662f36e32806596f8994cc0cff274
opielapatryk <xpatrykopiela@gmail.com>
2022-07-22 14:57:22 +0200

    contextmenu

--- js/contentmenu.js
+++ js/contentmenu.js
@@ -1,5 +1,16 @@
-
-//******* CUSTOM CONTEXT MENU *******//
-    //******* OPTION FOR CHANGING BACKGROUND COLOR *******//
-    //******* OPTION FOR CHANGING TEXT COLOR *******//
-    //******* OPTION TO ADD EXCERCISE FROM LIST *******//
+const menu = document.querySelector('menu')
+const scope = document.querySelector('body')
+scope.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+    const { clientX:mouseX,clientY:mouseY} = e
+    menu.style.top = `${mouseY}px`
+    menu.style.left = `${mouseX}px`
+    menu.classList.remove('visible')
+    setTimeout(()=>{menu.classList.add('visible')}) //settimeout is used to quit event loop, use this on arrow, or gen table
+    
+})
+scope.addEventListener('click',(e)=>{
+    if(e.target.offsetParent != menu){
+        menu.classList.remove('visible')
+    }
+})
