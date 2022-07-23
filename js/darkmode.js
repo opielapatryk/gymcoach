3b14b00113b83f631f96b9bb90eb5e1d249ce0e0
opielapatryk <xpatrykopiela@gmail.com>
2022-07-23 16:04:43 +0200

    enter

---
+++ js/darkmode.js
@@ -0,0 +1,19 @@
+const darkmode = document.querySelector('.dark-mode')
+const body = document.querySelector('body')
+const nav = document.querySelector('nav')
+let isDark = false
+darkmode.addEventListener('click',()=>{
+    if(!isDark){
+        body.style.background = 'linear-gradient(to right, rgb(9, 14, 73), rgb(11, 83, 11))'
+        nav.style.boxShadow = '#06193e 0 0 100px 5px'
+        nav.style.background = 'linear-gradient(to right, rgb(0, 43, 95), rgb(0, 71, 0))'
+        darkmode.style.color = 'black'
+        isDark = true
+    } else {
+        body.style.background = 'linear-gradient(to right, rgb(233, 235, 255), rgb(224, 255, 224))'
+        nav.style.boxShadow = '#b9d0ff 0 0 100px 5px'
+        nav.style.background = 'linear-gradient(to right, rgb(0, 115, 255), green)'
+        darkmode.style.color = 'white'
+        isDark = false
+    }
+})
