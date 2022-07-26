5b8d496fcab0cbc4a66b41fc276f5ffb2720f6fe
opielapatryk <xpatrykopiela@gmail.com>
2022-07-26 14:08:48 +0200

    fix

--- js/contextmenufunctons.js
+++ js/contextmenufunctons.js
@@ -1,10 +1,32 @@
 const item = document.querySelectorAll('.item')
 const color = document.querySelector('.color')
 const colorcontainer = document.querySelector('.selectcolor')
+
+
+//change cell color
 item[0].addEventListener("click",(e)=>{
     colorcontainer.style.display = 'inline'
+    
+    color.addEventListener('input',()=>{
+        if(focusedcell){
+        focusedcell.style.background = color.value 
+        }
+    });
 })
 
-color.addEventListener('input',()=>{
-    focusedcell.style.background = color.value 
-});
+
+
+//change table color
+item[1].addEventListener("click",(e)=>{
+    colorcontainer.style.display = 'inline'
+    color.removeEventListener('input',()=>{
+        focusedcell.style.background = color.value 
+    });
+    color.addEventListener('input',()=>{
+        td.forEach(element => {
+            element.style.background = color.value
+        });
+        colorcontainer.style.display = 'none'
+    });
+})
+
