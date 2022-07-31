28f218aa0a43cc571c707e55d515d0646b2fe14b
opielapatryk <xpatrykopiela@gmail.com>
2022-07-31 16:52:43 +0200

    l

--- js/addexc.js
+++ js/addexc.js
@@ -1,5 +1,17 @@
 const add = document.querySelectorAll('.add')
 const del = document.querySelectorAll('.del')
+const addinput = document.querySelector('.add-input')
+const addexcinput = document.querySelector('#addexc')
+const subaddexc = document.querySelector('#subaddexc')
+
 add[0].addEventListener('click', ()=>{
-    add[0].parentElement.parentElement.nextSibling.nextSibling.childNodes[1].textContent
+    addinput.style.display = 'block'
+})
+subaddexc.addEventListener('click', ()=>{
+    let newchild = document.createElement('li')
+    newchild.setAttribute('draggable', 'true')
+    newchild.classList.add('move')
+    newchild.innerText = addexcinput.value
+    add[0].parentElement.parentElement.nextSibling.nextSibling.appendChild(newchild)
+    addinput.style.display = 'none'
 })
