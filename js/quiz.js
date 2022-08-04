dfc7c018fc1ec72656c423a82e8ec5f749c3f93c
opielapatryk <xpatrykopiela@gmail.com>
2022-08-04 16:44:30 +0200

    e

--- js/quiz.js
+++ js/quiz.js
@@ -14,8 +14,13 @@ question.innerHTML = questions[0]
 let counter = 0 
 answers.addEventListener('click',()=>{
     counter += 1
-    question.innerHTML = questions[counter]
-    answers.innerHTML = answersList[0].join(' ')
+    if(counter <= 5){
+        question.innerHTML = questions[counter]
+        answers.innerHTML = answersList[counter].join(' ')
+    } else{
+        question.innerHTML = 'You have completed test!'
+        answers.innerHTML = 'Click there to generate your wourkout routine!! <button>Click</button>'
+    }
 })
 
 const answersList = 
@@ -28,3 +33,4 @@ const answersList =
         ['<button>Less than 2</button>', '<button>Less than 5</button>', '<button>Less than 9</button>', '<button>Less than 12</button>']
     ]
 
+answers.innerHTML = answersList[0].join(' ')
